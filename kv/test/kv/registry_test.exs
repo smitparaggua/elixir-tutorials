9e83d12d7d6ae62f79cccc9e49532fa4779257a9
John Smith Paraggua <smitparaggua@gmail.com>
2017-12-01 15:20:16 +0800

    Registry before monitoring

---
+++ kv/test/kv/registry_test.exs
@@ -0,0 +1,28 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  alias KV.Registry
+  alias KV.Bucket
+
+  setup do
+    {:ok, registry} = start_supervised Registry
+    %{registry: registry}
+  end
+
+  test "spawn buckets", %{registry: registry} do
+    assert Registry.lookup(registry, "shopping") == :error
+
+    Registry.create(registry, "shopping")
+    assert {:ok, bucket} = Registry.lookup(registry, "shopping")
+
+    Bucket.put(bucket, "milk", 1)
+    assert Bucket.get(bucket, "milk") == 1
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    Registry.create(registry, "shopping")
+    {:ok, bucket} = Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert Registry.lookup(registry, "shopping") == :error
+  end
+end
