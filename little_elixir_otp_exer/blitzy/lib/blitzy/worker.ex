3778a1bdf620e0edeb54230877fdd2832315bd1d
Smith Paraggua <smitparaggua@gmail.com>
2017-12-31 23:50:23 +0800

    Added blitzy (Distribution and Load balancing)

---
+++ little_elixir_otp_exer/blitzy/lib/blitzy/worker.ex
@@ -0,0 +1,29 @@
+defmodule Blitzy.Worker do
+  use Timex
+  require Logger
+
+  def start(url, func \\ &HTTPoison.get/1) do
+    IO.puts("Running on #node-#{node()}")
+    {timestamp, response} = Duration.measure(fn -> func.(url) end)
+    handle_response({Duration.to_milliseconds(timestamp), response})
+  end
+
+  defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
+  when code >= 200 and code <= 304 do
+    Logger.info("worker [#{worker_name()}] completed in #{msecs} msecs")
+    {:ok, msecs}
+  end
+
+  defp handle_response({_msecs, {:error, reason}}) do
+    Logger.info "worker [#{worker_name()}] error due to #{inspect(reason)}"
+    {:error, reason}
+  end
+
+  defp handle_response({_msecs, _}) do
+    Logger.info "worker [#{worker_name()}] errored out"
+    {:error, :unknown}
+  end
+
+  defp worker_name, do: "#{node()}-#{inspect(self())}"
+
+end
