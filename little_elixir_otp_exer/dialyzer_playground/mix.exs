a41bcddaee83199c7afb0732406e45d16b184290
John Smith Paraggua <smitparaggua@gmail.com>
2018-01-05 14:13:38 +0800

    Added dialyzer_playground

---
+++ little_elixir_otp_exer/dialyzer_playground/mix.exs
@@ -0,0 +1,27 @@
+defmodule DialyzerPlayground.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :dialyzer_playground,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+    ]
+  end
+end
