6352527f6a261adca3519a3b789d68c9e850c442
Smith Paraggua <smitparaggua@gmail.com>
2018-02-18 23:01:50 +0800

    [Rumbl] Added password hashing

--- programming_phoenix/rumbl/lib/rumbl/accounts/user.ex
+++ programming_phoenix/rumbl/lib/rumbl/accounts/user.ex
@@ -11,9 +11,26 @@ defmodule Rumbl.Accounts.User do
     timestamps()
   end
 
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params) do
     model
     |> cast(params, ~w(name username), empty_values: [])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password))
+    |> validate_length(:password, min: 6, max: 600)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Pbkdf2.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
