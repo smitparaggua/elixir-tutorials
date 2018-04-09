f9a14c1e57336e6bb3518e55e8181d3f9cc98f94
John Smith Paraggua <smitparaggua@gmail.com>
2018-04-09 23:08:16 +0800

    Added counter test

--- programming_phoenix/rumbl/lib/rumbl/application.ex
+++ programming_phoenix/rumbl/lib/rumbl/application.ex
@@ -1,23 +1,15 @@
 defmodule Rumbl.Application do
   use Application
 
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
       supervisor(Rumbl.Repo, []),
-      # Start the endpoint when the application starts
       supervisor(RumblWeb.Endpoint, []),
-      # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
-      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+      worker(Rumbl.CounterGenServer, [5])
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
     Supervisor.start_link(children, opts)
   end
